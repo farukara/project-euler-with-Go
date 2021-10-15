52aacd9868ff9be0f39732167b3e7220bd7e4424
farukara <69348522+farukara@users.noreply.github.com>
2021-10-15 13:13:01 +0300

    added 016

---
+++ files/016 - Power digit sum.go
@@ -0,0 +1,29 @@
+package main
+//project euler 016 - Power digit sum
+
+import (
+    "fmt"
+    "time"
+    "math"
+    "strconv"
+)
+
+func main() {
+    start := time.Now()
+    number := strconv.FormatFloat(math.Pow(2, 1_000), 'E', 301, 64)
+    str := number[:1]+number[2:len(number)-5]
+    total := 0
+    for _, letter := range str {
+        // total += strconv.Atoi(letter)
+        fmt.Println(letter)
+        // total += strconv.Atoi(string(letter))
+        // fmt.Println(strconv.ParseInt(letter, 10, 64))
+    }
+    fmt.Println(number)
+    fmt.Printf("%T", number)
+    fmt.Println(str)
+    fmt.Printf("%T", str)
+    fmt.Println(total)
+    fmt.Printf("Time: %.3f", time.Since(start).Seconds())
+    
+}
