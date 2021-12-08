e2667b7431dda508c4ff2c188e8f937c77f2b4ef
farukara <69348522+farukara@users.noreply.github.com>
2021-12-08 11:01:23 +0300

    modded 031

--- files/031 - Coin Sums.go
+++ files/031 - Coin Sums.go
@@ -1,12 +1,5 @@
-//In the United Kingdom the currency is made up of pound (£) and pence (p). There are eight coins in general circulation:
-
-//1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
-//It is possible to make £2 in the following way:
-
-//1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
-//How many different ways can £2 be made using any number of coins?
-
 package main
+//Project Euler - 031 - Coin Sums
 
 import (
     "fmt"
@@ -15,29 +8,29 @@ import (
 
 func main() {
     start := time.Now()
-    target := 200
-    coins := []int{1,2,5,10,20,50,100,200}
+    ultimateTarget := 200
+    coins := [8]int{1,2,5,10,20,50,100,200}
     table := make([][]int, len(coins)+1)
+
     for i:=0; i<len(coins)+1;i++ {
         table[i] = append(table[i], 1)
     }
-    for i:=1;i<target+1;i++ {
+
+    for i:=1;i<ultimateTarget+1;i++ {
         table[0] = append(table[0], 0)
     }
+
     for i:=1;i<len(coins)+1;i++ {
-        for j:=1;j<target+1; j++ {
-            if j >= coins[i-1] {
-                table[i] = append(table[i], table[i-1][j]+table[i][j-coins[i-1]])
+        for currentTarget:=1;currentTarget<ultimateTarget+1; currentTarget++ {
+            currentCoin := coins[i-1]
+
+            if currentTarget-currentCoin < 0 {
+                table[i] = append(table[i], table[i-1][currentTarget])
             } else {
-                if j-coins[i-1] < 0 {
-                    table[i] = append(table[i], table[i-1][j])
-                } else {
-                table[i] = append(table[i], table[i][j-coins[i-1]] + table[i-1][j])
-                }
+            table[i] = append(table[i], table[i-1][currentTarget] + table[i][currentTarget-currentCoin])
             }
         }
     }
     fmt.Println("Time:", time.Since(start).Nanoseconds())
-    fmt.Println(table[len(coins)][target])
-    // fmt.Println(table)
+    fmt.Println(table[len(coins)][ultimateTarget])
 }
